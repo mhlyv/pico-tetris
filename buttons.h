620517aee628dd72ab0aa0f47c26e33db986ef3d
Mihály Vesztergombi <mihaly.veszter@gmail.com>
2021-05-13 01:23:54 +0200

    reassign pins

--- buttons.h
+++ buttons.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 
-#define UP_BUTTON_PIN 16
-#define LEFT_BUTTON_PIN 17
-#define RIGHT_BUTTON_PIN 18
-#define DOWN_BUTTON_PIN 19
+#define UP_BUTTON_PIN 18
+#define LEFT_BUTTON_PIN 19
+#define RIGHT_BUTTON_PIN 16
+#define DOWN_BUTTON_PIN 17
 
 #define RIGHT_BUTTON 1
 #define UP_BUTTON 2
@@ -14,7 +14,7 @@
 #define LEFT_BUTTON 8
 #define DROP_BUTTON (UP_BUTTON + DOWN_BUTTON)
 
-#define BUTTON_SAMPLE_RATE 70
+#define BUTTON_SAMPLE_RATE 100
 
 void buttons_init();
 uint8_t buttons_read();
